1f5dcdb87aa8f9f31b960ffa8958d6f3d671b94a
HasanCoder66 <hasanashraf874@gmail.com>
2024-02-11 17:17:45 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  let [count, setCount] = useState(5)
+  const [count, setCount] = useState(5)
   
 
   const addValueHandler = () => {
    
       setCount(count + 1)
-      
-      // setCount( prevCount => prevCount + 1)
-      // setCount( prevCount => prevCount + 1)
-      // setCount( prevCount => prevCount + 1)
      
     
   }
